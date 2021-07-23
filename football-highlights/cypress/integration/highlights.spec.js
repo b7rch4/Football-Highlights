3416d656cd179f42669e6dbe52943f16c3ebf04d
James Birchall <james.birchall4@btinternet.com>
2021-07-23 09:40:17 +0100

    Second test written

--- football-highlights/cypress/integration/highlights.spec.js
+++ football-highlights/cypress/integration/highlights.spec.js
@@ -3,4 +3,9 @@ describe("When I load the page", function() {
         cy.visit('/')
         cy.get("#heading").should("contain", "Football Highlights")
     })
+
+    it("Displays a sub-heading", function() {
+        cy.visit('/')
+        cy.get("#sub-heading").should("contain", "Select two teams to see the match highlights")
+    })
 })
